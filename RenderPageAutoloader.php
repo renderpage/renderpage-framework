c44ed280683d14ee7bddfc807ed7a3b906870685
RenderPage <mail@renderpage.org>
2016-02-09 12:44:36 +0300

    Added autoloader class

---
+++ RenderPageAutoloader.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Project: RenderPage
+ * File:    RenderPageAutoloader.php
+ *
+ * @link    http://www.renderpage.org/
+ * @author  Sergey Pershin <sergey dot pershin at hotmail dot com>
+ * @package RenderPage
+ * @version 1.0.0
+ */
+
+namespace renderpage\libs;
+
+/**
+ * This is Autoloader class
+ */
+class RenderPageAutoloader
+{
+    /**
+     * Autoload classes.
+     *
+     * @param string $class class name.
+     */
+    public static function autoload($class)
+    {
+        $filename = dirname(APP_DIR) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+        if (file_exists($filename)) {
+            include_once $filename;
+        }
+    }
+}
