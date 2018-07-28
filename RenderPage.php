ced3561180f2d5dec89ccb4a7bcfefedbd675453
Sergey Pershin <sergey.pershin@hotmail.com>
2018-07-28 15:50:20 +0300

    require

--- RenderPage.php
+++ RenderPage.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Sergey Pershin <sergey dot pershin at hotmail dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * Project: RenderPage
  * File:    RenderPage.php
@@ -11,60 +35,6 @@
  */
 
 namespace renderpage\libs;
-
-/**
- * Debugging mode.
- */
-if (!defined('RENDERPAGE_DEBUG')) {
-    define('RENDERPAGE_DEBUG', false);
-}
-
-/**
- * Absolute path to RenderPage library files.
- */
-if (!defined('RENDERPAGE_DIR')) {
-    define('RENDERPAGE_DIR', __DIR__);
-}
-
-/**
- * Absolute path to application files.
- */
-if (!defined('APP_DIR')) {
-    define('APP_DIR', dirname(__DIR__) . '/app');
-}
-
-/**
- * Absolute path to compiled files.
- */
-if (!defined('COMPILE_DIR')) {
-    define('COMPILE_DIR', APP_DIR . '/compile');
-}
-
-/**
- * Load always needed external class files
- */
-require_once RENDERPAGE_DIR . '/traits/Singleton.php';
-require_once RENDERPAGE_DIR . '/RenderPageAutoloader.php';
-require_once RENDERPAGE_DIR . '/RenderPageException.php';
-require_once RENDERPAGE_DIR . '/Request.php';
-require_once RENDERPAGE_DIR . '/Route.php';
-require_once RENDERPAGE_DIR . '/Session.php';
-require_once RENDERPAGE_DIR . '/Language.php';
-require_once RENDERPAGE_DIR . '/DB.php';
-require_once RENDERPAGE_DIR . '/Model.php';
-require_once RENDERPAGE_DIR . '/View.php';
-require_once RENDERPAGE_DIR . '/Controller.php';
-
-/**
- * Autoloader
- */
-spl_autoload_register(['\renderpage\libs\RenderPageAutoloader', 'autoload']);
-
-/**
- * Errors
- */
-set_exception_handler(['\renderpage\libs\RenderPageException', 'exceptionHandler']);
-set_error_handler(['\renderpage\libs\RenderPageException', 'errorHandler']);
 
 /**
  * This is the main RenderPage class
@@ -132,26 +102,26 @@ class RenderPage {
     /**
      * Routing
      *
-     * @return boolean
+     * @return \renderpage\libs\RenderPage
      */
-    public function route() {
+    public function route(): RenderPage {
         $this->route = new Route;
-
         $this->route->run();
 
-        if ($this->route->controllerName != '') {
-            $controllerName = '\app\controllers\\' . $this->route->controllerName;
-            $this->controller = new $controllerName;
-            return true;
+        if ($this->route->controllerName) {
+            $class = '\app\controllers\\' . $this->route->controllerName;
+            $this->controller = new $class;
         }
 
-        return false;
+        return $this;
     }
 
     /**
      * Application execute
+     *
+     * @return \renderpage\libs\RenderPage
      */
-    public function execute() {
+    public function execute(): RenderPage {
         if (!empty($this->controller)) {
             // Before action
             $this->controller->before();
@@ -162,19 +132,21 @@ class RenderPage {
             // After action
             $this->controller->after();
         }
+
+        return $this;
     }
 
     /**
      * Outputting data
      */
     public function output() {
-        if ($this->outputData === false) {
+        if (false === $this->outputData) {
             header('Content-Type: text/html; charset=' . self::$charset, true, 404);
             $view = new View;
             $view->title = '404';
             echo $view->render('404', 'error');
         } elseif (is_array($this->outputData)) {
-            header('Content-Type: application/json');
+            header('Content-Type: application/json; charset=UTF-8');
             echo json_encode($this->outputData);
         } else {
             header('Content-Type: text/html; charset=' . self::$charset);
---
+++ require.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Sergey Pershin <sergey dot pershin at hotmail dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Debugging mode.
+ */
+if (!defined('RENDERPAGE_DEBUG')) {
+    define('RENDERPAGE_DEBUG', false);
+}
+
+/**
+ * Absolute path to RenderPage library files.
+ */
+if (!defined('RENDERPAGE_DIR')) {
+    define('RENDERPAGE_DIR', __DIR__);
+}
+
+/**
+ * Absolute path to application files.
+ */
+if (!defined('APP_DIR')) {
+    define('APP_DIR', dirname(__DIR__) . '/app');
+}
+
+/**
+ * Absolute path to compiled files.
+ */
+if (!defined('COMPILE_DIR')) {
+    define('COMPILE_DIR', APP_DIR . '/compile');
+}
+
+/**
+ * Load always needed external class files
+ */
+require_once RENDERPAGE_DIR . '/traits/Singleton.php';
+require_once RENDERPAGE_DIR . '/RenderPageException.php';
+require_once RENDERPAGE_DIR . '/Request.php';
+require_once RENDERPAGE_DIR . '/Route.php';
+require_once RENDERPAGE_DIR . '/Session.php';
+require_once RENDERPAGE_DIR . '/Language.php';
+require_once RENDERPAGE_DIR . '/DB.php';
+require_once RENDERPAGE_DIR . '/Model.php';
+require_once RENDERPAGE_DIR . '/View.php';
+require_once RENDERPAGE_DIR . '/Controller.php';
+
+/**
+ * Autoloader
+ */
+spl_autoload_register(function ($class) {
+    if (0 === strpos($class, 'renderpage\lib')) {
+        $filename = RENDERPAGE_DIR . DIRECTORY_SEPARATOR . substr(strtr($class, '\\', DIRECTORY_SEPARATOR), strlen('renderpage\lib') + 1) . '.php';
+    } else {
+        $filename = dirname(APP_DIR) . DIRECTORY_SEPARATOR . strtr($class, '\\', DIRECTORY_SEPARATOR) . '.php';
+    }
+    include_once $filename;
+});
+
+/**
+ * Errors
+ */
+set_exception_handler(['\renderpage\libs\RenderPageException', 'exceptionHandler']);
+set_error_handler(['\renderpage\libs\RenderPageException', 'errorHandler']);
