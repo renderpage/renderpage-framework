39fee28251fb879092e05b0d8cd7fbcf01aba2a3
Sergey Pershin <sergey.pershin@hotmail.com>
2018-07-28 11:58:36 +0300

    Added DB::

--- DB.php
+++ DB.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Sergey Pershin <sergey dot pershin at hotmail dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * Project: RenderPage
  * File:    DB.php
@@ -12,6 +36,8 @@
 
 namespace renderpage\libs;
 
+use PDO;
+
 /**
  * This is DB class
  */
@@ -21,6 +47,19 @@ class DB {
      * Singleton trait
      */
     use traits\Singleton;
+
+    /**
+     * Database table prefix.
+     *
+     * @var string
+     */
+    public static $tablePrefix = '';
+
+    /**
+     *
+     * @var array
+     */
+    private $cfg;
 
     /**
      * Instance of PDO class
@@ -36,17 +75,66 @@ class DB {
      */
     private $isConnected = false;
 
-    /**
-     * Connect to DB
-     */
-    private function connect() {
-        $conf = include APP_DIR . '/conf/db.php';
-        try {
-            $this->dbh = new \PDO($conf['dsn'], $conf['username'], $conf['password'], [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$conf['charset']}"]);
-            $this->isConnected = true;
-        } catch (RenderPageException $e) {
-            echo 'Unable to connect: ' . $e->getMessage();
+    private function __construct() {
+        $this->cfg = include APP_DIR . '/conf/db.php';
+        if (isset($this->cfg['tablePrefix'])) {
+            self::$tablePrefix = $this->cfg['tablePrefix'];
         }
+    }
+
+    /**
+     * Get array
+     *
+     * @param string $sql
+     * @param array $inputParameters
+     *
+     * @return array
+     */
+    public function getArray(string $sql, array $inputParameters = []) {
+        $sth = $this->query($sql, $inputParameters);
+        return $sth->fetchAll();
+    }
+
+    /**
+     * Get one
+     *
+     * @param string $sql
+     * @param array $inputParameters
+     *
+     * @return mixed
+     */
+    public function getOne(string $sql, array $inputParameters = []) {
+        $row = $this->getRow($sql, $inputParameters);
+        return $row ? $row[0] : false;
+    }
+
+    /**
+     * Get row
+     *
+     * @param string $sql
+     * @param array $inputParameters
+     *
+     * @return array
+     */
+    public function getRow(string $sql, array $inputParameters = []) {
+        $sth = $this->query($sql, $inputParameters);
+        return $sth->fetch();
+    }
+
+    /**
+     * Insert
+     *
+     * @param string $into table name
+     * @param array $data data
+     *
+     * @return int insert id
+     */
+    public function insert(string $into, array $data): int {
+        $sql = 'INSERT INTO `' . str_replace('.', '`.`', $into) . '` (`';
+        $sql .= implode('`, `', array_keys($data)) . '`) VALUES (';
+        $sql .= implode(', ', array_fill(0, count($data), '?')) . ')';
+        $this->query($sql, array_values($data));
+        return $this->dbh->lastInsertId();
     }
 
     /**
@@ -69,66 +157,6 @@ class DB {
     }
 
     /**
-     * Get array
-     *
-     * @param string $sql
-     * @param array $inputParameters
-     *
-     * @return array
-     */
-    public function getArray(string $sql, array $inputParameters = []) {
-        $sth = $this->query($sql, $inputParameters);
-        return $sth->fetchAll();
-    }
-
-    /**
-     * Get row
-     *
-     * @param string $sql
-     * @param array $inputParameters
-     *
-     * @return array
-     */
-    public function getRow(string $sql, array $inputParameters = []) {
-        $sth = $this->query($sql, $inputParameters);
-        return $sth->fetch();
-    }
-
-    /**
-     * Get one
-     *
-     * @param string $sql
-     * @param array $inputParameters
-     *
-     * @return mixed
-     */
-    public function getOne(string $sql, array $inputParameters = []) {
-        $row = $this->getRow($sql, $inputParameters);
-
-        if (!$row) {
-            return false;
-        }
-
-        return $row[0];
-    }
-
-    /**
-     * Insert
-     *
-     * @param string $into table name
-     * @param array $data data
-     *
-     * @return int insert id
-     */
-    public function insert(string $into, array $data): int {
-        $sql = 'INSERT INTO `' . str_replace('.', '`.`', $into) . '` (`';
-        $sql .= implode('`, `', array_keys($data)) . '`) VALUES (';
-        $sql .= implode(', ', array_fill(0, count($data), '?')) . ')';
-        $this->query($sql, array_values($data));
-        return $this->dbh->lastInsertId();
-    }
-
-    /**
      * Truncate table
      *
      * @param string $table table name
@@ -136,6 +164,18 @@ class DB {
     public function truncate(string $table) {
         $sql = 'TRUNCATE TABLE `' . str_replace('.', '`.`', $table) . '`';
         $this->query($sql);
+    }
+
+    /**
+     * Connect to DB
+     */
+    private function connect() {
+        try {
+            $this->dbh = new PDO($this->cfg['dsn'], $this->cfg['username'], $this->cfg['password'], [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->cfg['charset']}"]);
+            $this->isConnected = true;
+        } catch (RenderPageException $e) {
+            echo 'Unable to connect: ' . $e->getMessage();
+        }
     }
 
 }
